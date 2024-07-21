1cb0583a2bb46f62d6cecb0d7c8473f475466e07
Ashwini Somnath Unde <146004602+ashwiniunde@users.noreply.github.com>
2024-07-21 14:32:10 +0530

    Basic.py

---
+++ Basic.py
@@ -0,0 +1,56 @@
+print("Hello World!!")
+
+x=1
+print(x)
+print(type(x)) #it print type of x
+
+#Taking input from user 
+age=int(input("Enter age::"))
+print("Age is :",age)
+print(type(age))
+
+age1=int(input("Enter age::"))
+print("Age is :",age1)
+print(type(age1))
+
+print(age+age1) #Here addition of age and age1 will be son
+
+#Int to float conversation 
+a=20
+print(type(a))
+b=float(a)
+print(b)
+print(type(b))
+
+#string to float conversation 
+c="20.05"
+print(type(a))
+d=float(c)
+print(d)
+print(type(d))
+
+#complex number a+jb
+c=12
+c1=5j
+print(c+c1)
+print(type(c))
+print(type(c1))
+print(type(c+c1))
+print(c.real)
+print(c1.imag)
+
+#Arithmetic Operations
+a=5
+b=10
+#Addition
+print(a+b)
+print(type(a+b))
+#Substraction
+print(a-b)
+print(type(a-b))
+#Multiplication
+print(a*b)
+print(type(a*b))
+#Division
+print(a/b)
+print(type(a/b))
